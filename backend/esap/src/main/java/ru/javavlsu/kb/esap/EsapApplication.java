0c62feaf6acdbf421e23abaacd7dc5dbb9190c45
thewyolar <thewyolar@gmail.com>
2023-02-16 19:12:14 +0300

    Initial commit

---
+++ backend/esap/src/main/java/ru/javavlsu/kb/esap/EsapApplication.java
@@ -0,0 +1,13 @@
+package ru.javavlsu.kb.esap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsapApplication.class, args);
+    }
+
+}
