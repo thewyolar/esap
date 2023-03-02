889002aea9afa3a30381d6314ea52ef387153125
thewyolar <thewyolar@gmail.com>
2023-03-02 12:43:11 +0300

    Create app architecture

--- backend/esap/src/main/java/ru/javavlsu/kb/esap/EsapApplication.java
+++ backend/esap/src/main/java/ru/javavlsu/kb/esap/EsapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EsapApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EsapApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EsapApplication.class, args);
+	}
 
 }
