af9404812e8dd57072c8125aedbeeb02d5eb0cd8
thewyolar <thewyolar@gmail.com>
2023-03-31 13:17:26 +0300

    refactor: change dummy data

---
+++ frontend/esap-admin/src/dummyData.ts
@@ -0,0 +1,181 @@
+export const userData = [
+  {
+    name: 'Jan',
+    'Active User': 4000
+  },
+  {
+    name: 'Feb',
+    'Active User': 2500
+  },
+  {
+    name: 'Mar',
+    'Active User': 2100
+  },
+  {
+    name: 'Apr',
+    'Active User': 3200
+  },
+  {
+    name: 'May',
+    'Active User': 5000
+  },
+  {
+    name: 'Jun',
+    'Active User': 4300
+  },
+  {
+    name: 'Jul',
+    'Active User': 1000
+  },
+  {
+    name: 'Agu',
+    'Active User': 200
+  },
+  {
+    name: 'Sep',
+    'Active User': 1700
+  },
+  {
+    name: 'Oct',
+    'Active User': 7000
+  },
+  {
+    name: 'Nov',
+    'Active User': 9000
+  },
+  {
+    name: 'Dec',
+    'Active User': 5000
+  }
+]
+
+export const patientsData = [
+  {
+    id: 1,
+    firstName: 'Иван',
+    patronymic: 'Иванович',
+    lastName: 'Иванов',
+    birthDate: '01.01.1980',
+    gender: 'Мужской',
+    address: 'ул. Ленина, 1, кв. 10',
+    phoneNumber: '+7 (123) 456-78-90',
+    email: 'ivan.ivanov@example.com'
+  },
+  {
+    id: 2,
+    firstName: 'Мария',
+    patronymic: 'Александровна',
+    lastName: 'Петрова',
+    birthDate: '15.05.1975',
+    gender: 'Женский',
+    address: 'ул. Пушкина, 2, кв. 20',
+    phoneNumber: '+7 (987) 654-32-10',
+    email: 'maria.petrova@example.com'
+  }
+]
+
+export const productsRows = [
+  {
+    id: 1,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 2,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 3,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 4,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 5,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 6,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 7,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 8,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 9,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 10,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  },
+  {
+    id: 11,
+    name: 'Apple Airpods',
+    img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1632861342000',
+    stock: 123,
+    status: 'active',
+    price: '$120.00'
+  }
+]
+
+export const productData = [
+  {
+    name: 'Jan',
+    Sales: 4000
+  },
+  {
+    name: 'Feb',
+    Sales: 2500
+  },
+  {
+    name: 'Mar',
+    Sales: 2100
+  }
+]
