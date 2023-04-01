769ce27eda3af780ccd5364cb76ee94162b7c445
thewyolar <thewyolar@gmail.com>
2023-04-01 19:06:11 +0300

    refactor: update http service to send request for doctor and clinic registration

--- frontend/esap-admin/src/service/HttpService.tsx
+++ frontend/esap-admin/src/service/HttpService.tsx
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { clinicRegistrationDTO } from "../model/dto/ClinicRegistrationDTO";
+import { ClinicRegistrationDTO } from "../model/dto/ClinicRegistrationDTO";
 
-
-export default class HttpService{
+class HttpService {
 
     private static url = "http://localhost:8080";
 
-    public static async registrationClinics(body: clinicRegistrationDTO){
-        return await axios.post(HttpService.url + "/api/clinic/", body)
+    public static async registrationClinics(body: ClinicRegistrationDTO){
+        return await axios.post(HttpService.url + "/api/clinic", body)
         .then((res) => res.data)
         .catch((error) => error)
     }
-
 }
+
+export default HttpService;
