a413234d465b408d1e59c76b374347d58df48e40
thewyolar <thewyolar@gmail.com>
2023-04-06 22:12:48 +0300

    feat: add spring doc

--- backend/esap/src/main/java/ru/javavlsu/kb/esap/EsapApplication.java
+++ backend/esap/src/main/java/ru/javavlsu/kb/esap/EsapApplication.java
@@ -1,9 +1,7 @@
 package ru.javavlsu.kb.esap;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EsapApplication {
@@ -11,10 +9,4 @@ public class EsapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EsapApplication.class, args);
 	}
-
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
-
 }
