7e509c843f44fa6e496ea8904fc024263ed5dabf
thewyolar <thewyolar@gmail.com>
2023-04-07 13:17:55 +0300

    fix: add spring doc config

---
+++ backend/esap/src/main/java/ru/javavlsu/kb/esap/config/SpringDocConfig.java
@@ -0,0 +1,26 @@
+package ru.javavlsu.kb.esap.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI().info(new Info()
+                .title("ЕСАП")
+                .version("1.0.0")
+                .description("Единая система автоматизации поликлиник"));
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
