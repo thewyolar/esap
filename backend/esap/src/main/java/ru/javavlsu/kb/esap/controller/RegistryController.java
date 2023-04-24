45643bf860298610f6d28aa60cdb1e8868694abd
Dalv <vladbulanov47@gmail.com>
2023-04-24 12:20:18 +0300

    refactor: constructor

--- backend/esap/src/main/java/ru/javavlsu/kb/esap/controller/RegistryController.java
+++ backend/esap/src/main/java/ru/javavlsu/kb/esap/controller/RegistryController.java
@@ -1,6 +1,5 @@
 package ru.javavlsu.kb.esap.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.javavlsu.kb.esap.model.Registry;
 import ru.javavlsu.kb.esap.service.RegistryService;
@@ -10,8 +9,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class RegistryController {
-    @Autowired
-    private RegistryService registryService;
+
+    private final RegistryService registryService;
+
+    public RegistryController(RegistryService registryService) {
+        this.registryService = registryService;
+    }
 
     @GetMapping("/registries")
     public List<Registry> getAllRegistries() { return registryService.getAll(); }
