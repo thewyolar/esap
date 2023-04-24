d40c549081f9f16d20814facb22b9335f02d8f9b
Dalv <vladbulanov47@gmail.com>
2023-04-24 12:26:44 +0300

    add: return /login

--- frontend/esap-admin/src/service/auth/Api.tsx
+++ frontend/esap-admin/src/service/auth/Api.tsx
@@ -14,7 +14,9 @@ Api.interceptors.response.use(
   (response) => response,
   (error) => {
     console.log(error);
-    window.location.href = "/login";
+    if (error.response.status === 401) {
+      window.location.href = "/login";
+    }
     return Promise.reject(error);
   }
 );
