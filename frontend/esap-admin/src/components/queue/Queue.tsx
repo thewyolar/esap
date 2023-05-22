a73877afd49eb760537b9d54965d2aa3ea0180a0
thewyolar <thewyolar@gmail.com>
2023-05-22 23:41:14 +0300

    refactor: update queue

--- frontend/esap-admin/src/components/queue/Queue.tsx
+++ frontend/esap-admin/src/components/queue/Queue.tsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Grid, Card, CardContent, Typography, List, ListItem, ListItemAvatar, Avatar, ListItemText} from "@mui/material";
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import {Schedule} from '../../model/Schedule';
 import HttpService from '../../service/HttpService';
 import {Appointment} from "../../model/Appointment";
-import { Link } from 'react-router-dom';
+import {Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineOppositeContent, timelineOppositeContentClasses, TimelineSeparator} from "@mui/lab";
+import {Grid, Typography} from "@mui/material";
+import moment from "moment";
 
 const Queue: React.FC = () => {
   const { scheduleId } = useParams<{ scheduleId: string }>();
@@ -12,36 +13,58 @@ const Queue: React.FC = () => {
 
   useEffect(() => {
     if (scheduleId) {
-      HttpService.getScheduleByIdAndDoctor(parseInt(scheduleId))
+      HttpService.getScheduleById(parseInt(scheduleId))
         .then((response) => setSchedule(response))
         .catch((error) => console.error(error));
     }
   }, [scheduleId]);
 
+  const formatTime = (timeString: string) => {
+    const date = new Date(timeString);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  };
+
   return (
-    <Grid container className="queue" spacing={2} justifyContent="center">
-      <Grid item xs={12} sm={8} md={6}>
-        <Card raised>
-          <CardContent>
-            <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
-              Электронная очередь
-            </Typography>
-            <List sx={{ display: 'flex', flexDirection: 'column'}}>
-              {schedule?.appointments.map((appointment: Appointment, index: number) => (
-                <ListItem key={index} disablePadding>
-                  <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: 'primary.main' }}>{index + 1}</Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={`${appointment.patient.lastName} ${appointment.patient.firstName}`}
-                    secondary={`Время записи: ${appointment.startAppointments}`}
-                  />
-                  <Link to={`/editingMedicalCard/${appointment.patient.id}`}>Карта</Link>
-                </ListItem>
-              ))}
-            </List>
-          </CardContent>
-        </Card>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h2" sx={{ m: 2 }}>
+          Журнал приемов
+        </Typography>
+        <Timeline
+          sx={{
+            [`& .${timelineOppositeContentClasses.root}`]: {
+              flex: 'unset',
+              paddingRight: '16px',
+            },
+          }}
+        >
+          {schedule?.appointments.map((appointment: Appointment, index: number) => (
+            <TimelineItem key={index}>
+              <TimelineOppositeContent
+                sx={{ flex: 0.2 }}
+                align="right"
+                variant="body2"
+                color="text.secondary"
+              >
+                {moment(appointment.startAppointments, "HH:mm:ss").format("HH:mm")}
+              </TimelineOppositeContent>
+              <TimelineSeparator>
+                <TimelineDot variant="outlined" sx={{ margin: '0' }} />
+                {index !== schedule.appointments.length - 1 && <TimelineConnector />}
+              </TimelineSeparator>
+              <TimelineContent sx={{ px: 2 }}>
+                <Typography variant="h6" component="span" sx={{ fontSize: '16px', fontWeight: 'bold' }} color="primary">
+                  {`${appointment.patient.lastName} ${appointment.patient.firstName}`}
+                </Typography>
+                <Link to={`/editingMedicalCard/${appointment.patient.id}`}>
+                  <Typography variant="body2" color="text.secondary">
+                    Мед. карта
+                  </Typography>
+                </Link>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
+        </Timeline>
       </Grid>
     </Grid>
   );
